97c484de778f5ccf85749a2b53adebbe87af3cc0
Aino <76224883+ainovi@users.noreply.github.com>
2021-05-25 21:07:53 +0100

    Removed the header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello World</header>
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
